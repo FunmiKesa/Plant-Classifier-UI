f9d7c35dd0ef69262f67684b76d8382ba49661b1
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-04-16 00:05:03 +0200

    Removed favicon from template.

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app.index_string = '''
 
         <title>Plant Classifier</title>
 
-        {%favicon%}
+        
         <link href="https://codepen.io/chriddyp/pen/bWLwgP.css" rel="stylesheet">
         <link href="https://codepen.io/chriddyp/pen/brPBPO.css" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
